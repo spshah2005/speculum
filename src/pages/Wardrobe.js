fd9fdcd444b4560f0a7a3547b65834cfd2f2aa57
shruti <shrutish@umich.edu>
2024-08-06 09:51:39 -0400

    carousel functionality & connected upload

--- src/pages/Wardrobe.js
+++ src/pages/Wardrobe.js
@@ -1,7 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "../styles/Wardrobe.css";
 import Carousel from "../components/CenterMode"
 import WardrobeUpload from "../components/WardrobeUpload";
+
+//firebase
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
+
+//auth
+import { useAuth } from "../context/AuthContext"
 
 const Wardrobe = () => {
   const [openSections, setOpenSections] = useState({
@@ -9,6 +16,66 @@ const Wardrobe = () => {
     Bottoms: false,
     Accessories: false,
   });
+
+  const [tops, setTops] = useState(new Map());
+  const [bottoms, setBottoms] = useState(new Map());
+  const [accessories, setAccessories] = useState(new Map());
+
+  const {currentUser} = useAuth()
+
+  const addTop = (key, item) => {
+    setTops((prevTops) => {
+      const newTops = new Map(prevTops);
+      newTops.set(key, item);
+      return newTops;
+    });
+  };
+
+  const addBottom = (key, item) => {
+    setBottoms((prevBottoms) => {
+      const newBottoms = new Map(prevBottoms);
+      newBottoms.set(key, item);
+      return newBottoms;
+    });
+  };
+
+  const addAccessory = (key, item) => {
+    setAccessories((prevAccessories) => {
+      const newAccessories = new Map(prevAccessories);
+      newAccessories.set(key, item);
+      return newAccessories;
+    });
+  };
+
+  useEffect(() => {
+    const db = firebase.firestore();
+    const docRef = db.collection('users').doc(currentUser.uid)
+    async function checkIfExists() {
+      const userDoc = await docRef.get();
+      return userDoc.exists
+    }
+    if (checkIfExists()) {
+      docRef.collection('wardrobe').get()
+      .then(snapshot => {
+        snapshot.docs.forEach(doc => {
+            const userData = doc.data()
+            if (userData.type.toLowerCase() === "top"){
+              addTop(doc.id, {colors:userData.colors, styles:userData.styles, title:userData.title, imgUrl:userData.imgUrl})
+            }
+            else if (userData.type.toLowerCase() === "bottom"){
+              addBottom(doc.id, {colors:userData.colors, styles:userData.styles, title:userData.title, imgUrl:userData.imgUrl})
+            }
+            else if (userData.type.toLowerCase() === "accessory"){
+              addAccessory(doc.id, {colors:userData.colors, styles:userData.styles, title:userData.title, imgUrl:userData.imgUrl})
+            }
+        });
+      })
+      .catch(error => {
+        console.error("Error getting documents: ", error);
+      });
+    }
+
+  }, [])
 
   const toggleSection = (section) => {
     setOpenSections((prevState) => ({
@@ -39,7 +106,9 @@ const Wardrobe = () => {
               {openSections[section] ? "close" : `${section}`}
             </button>
             <div className="wardrobe-content">
-              <Carousel />
+              {section==="tops" && <Carousel content={tops}/>}
+              {section==="bottoms" && <Carousel content={bottoms}/>}
+              {section==="accessories" && <Carousel content={accessories}/>}
             </div>
           </div>
         ))}
